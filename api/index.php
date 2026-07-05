dee3aaa566b4234c4bf71d000aed9c519500d01a
Kiiiyyy <dzakyramadhani14510@gmail.com>
2026-07-05 22:58:49 +0700

    fix: bypass vercel read only filesystem using tmp directory

--- api/index.php
+++ api/index.php
@@ -1,2 +1,17 @@
 <?php
+
+// 1. Paksa Vercel membuat folder temporary yang writable di dalam runtime Lambda
+$writableFolders = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions'
+];
+
+foreach ($writableFolders as $folder) {
+    if (!is_dir($folder)) {
+        mkdir($folder, 0777, true);
+    }
+}
+
+// 2. Jalankan index utama Laravel seperti biasa
 require __DIR__ . '/../public/index.php';
