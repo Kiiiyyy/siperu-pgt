cc9986e5ecce0f96db8e3b420c924a46ebfea70c
Kiiiyyy <dzakyramadhani14510@gmail.com>
2026-07-05 23:09:52 +0700

    fix: enforce root working directory and server variables for vercel

--- api/index.php
+++ api/index.php
@@ -1,10 +1,18 @@
 <?php
 
-// 1. Paksa Vercel membuat folder temporary yang writable di dalam runtime Lambda
+// 1. PAKSA PHP pindah ke folder utama (Root), bukan di dalam folder api! (Krusial Banget)
+chdir(__DIR__ . '/../');
+
+// 2. Manipulasi server variable agar Laravel mengira dia berjalan normal dari public/index.php
+$_SERVER['SCRIPT_NAME'] = '/index.php';
+$_SERVER['SCRIPT_FILENAME'] = __DIR__ . '/../public/index.php';
+
+// 3. Amankan folder temporary untuk cache & session di Vercel (Bypass Read-Only)
 $writableFolders = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
-    '/tmp/storage/framework/sessions'
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs'
 ];
 
 foreach ($writableFolders as $folder) {
@@ -13,5 +21,5 @@ foreach ($writableFolders as $folder) {
     }
 }
 
-// 2. Jalankan index utama Laravel seperti biasa
+// 4. Panggil index utama Laravel yang asli
 require __DIR__ . '/../public/index.php';
